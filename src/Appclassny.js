57703f70224c9c6487d2c0853e44b97dc8f7aba4
edchenstudio1000 <42553653+edchenstudio1000@users.noreply.github.com>
2018-10-14 20:33:55 -0400

    Update Appclassny.js

--- src/Appclassny.js
+++ src/Appclassny.js
@@ -27,9 +27,11 @@ componentDidMount() {
       usersb:function(){//receivers array
         //this is the way to range and two arrays 
         //receivers and givers in order to make it work
-        
+          var n = Math.floor(Math.random() * result.users.length-1);
+        window.alert(n);
+        for(var i=0;i<n;i++) {
         var lastuser = result.users.pop();
-        result.users.unshift(lastuser);
+        result.users.unshift(lastuser);}
         return result.users;
       }
     });
